ec46656aa3b85c3aa0eebcd37057896522a928df
games647 <games647@users.noreply.github.com>
2016-09-02 11:23:26 +0200

    Hide the application menu by default

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
+  mainWindow.setMenu(null)
+
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
 
